97a337d2e1d39ef42d37ef75f09b624f56cf160e
Alex Ambarchian <alex@alexambarchian.com>
2021-05-08 22:45:02 -0400

    Clean up shutdown routine

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,23 @@ use ui::Ui;
 use ui::util::UiEvent;
 use app::App;
 use tokio::sync::mpsc::{Sender, Receiver, channel};
+use crossterm::terminal::{disable_raw_mode, LeaveAlternateScreen};
+use crossterm::execute;
+use std::io::stdout;
 
 #[tokio::main]
 async fn main() {
     let mut app = App::default();
-    let (tx, mut rx): (Sender<UiEvent>, Receiver<UiEvent>) = channel(15);
+    let (tx, rx): (Sender<UiEvent>, Receiver<UiEvent>) = channel(15);
 
     // Listen for keyboard events
-    tokio::spawn(event::listen_events(tx));
+    let handle = tokio::spawn(event::listen_events(tx));
 
     // Start UI
     let mut ui = Ui::new();
-    ui.listen(rx, &mut app);
+    ui.listen(rx, &mut app).await;
+    execute!(stdout(), LeaveAlternateScreen).unwrap();
+
+    handle.await.unwrap();
+    disable_raw_mode().unwrap();
 }
